52183b17914e4d65c35558847902452e3fdfe5ad
AbrahamWillemH <abraham120604@gmail.com>
2026-03-02 21:36:46 +0700

    cargo init

---
+++ target/debug/deps/rust_intro-6bb47df7f4b84a3d.d
@@ -0,0 +1,5 @@
+/home/hanbee/workspace/rust-intro/cargo/target/debug/deps/rust_intro-6bb47df7f4b84a3d.d: src/main.rs
+
+/home/hanbee/workspace/rust-intro/cargo/target/debug/deps/librust_intro-6bb47df7f4b84a3d.rmeta: src/main.rs
+
+src/main.rs:
---
+++ target/debug/rust-intro.d
@@ -0,0 +1,1 @@
+/home/hanbee/workspace/rust-intro/target/debug/rust-intro: /home/hanbee/workspace/rust-intro/src/main.rs
