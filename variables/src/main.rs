ce3106de3acd9fc1fe0ca3eb4b100a6857206816
AbrahamWillemH <abraham120604@gmail.com>
2026-03-02 23:14:59 +0700

    arrays and tuples (and jumped to for loops ehe)

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,16 @@
 fn main() {
-    let mut x = 0x77;
-    println!("The value of x is: {}", x);
-    x = 6;
-    println!("The value of x is: {}", x);
+    let tup = (500, 6.4, 1);
+    println!("The value of first element is: {}", tup.0);
+    println!("The value of second element is: {}", tup.1);
+    println!("The value of third element is: {}", tup.2);
+
+    let (x, y, z) = tup;
+    println!("The value of first element is: {}", x);
+    println!("The value of second element is: {}", y);
+    println!("The value of third element is: {}", z);
+
+    let array = [1, 2, 3, 4, 5];
+    for value in array {
+        println!("{}", value);
+    }
 }
